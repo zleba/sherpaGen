d5cf7276d06ff185ee78fedc1fa9a7b887699b92
Radek Zlebcik <zlebcr@mail.desy.de>
2019-11-12 17:27:46 +0100

    Changing seed

--- MY/PROJECT/test/runSherpa.sh
+++ MY/PROJECT/test/runSherpa.sh
@@ -35,7 +35,15 @@ cd sherpaProduction/pt${slice}_${tag}/$id
 
 #sed -i 's/CMS_2016_I1459051/'   sherpa_QCD_pt${slice}_${tag}_LO_13TeV_MASTER_GEN.py
 sed -i "s/int32(1000)/int32(${nEv})/"   sherpa_QCD_pt${slice}_${tag}_LO_13TeV_MASTER_cff_py_GEN.py
-sed -i "s/CMS_2016_I1459051/CMS_2016_I1459051/"   sherpa_QCD_pt${slice}_${tag}_LO_13TeV_MASTER_cff_py_GEN.py
+sed -i "s/CMS_2016_I1459051/CMS_2019_incJets/"   sherpa_QCD_pt${slice}_${tag}_LO_13TeV_MASTER_cff_py_GEN.py
+#sed -i "s/crossSection = cms.untracked.double(-1),/& seed = cms.untracked.int32(14936),/"    sherpa_QCD_pt${slice}_${tag}_LO_13TeV_MASTER_cff_py_GEN.py
+
+seed=11$id
+sed -i "/Configuration.StandardSequences.FrontierConditions_GlobalTag_cff/aprocess.RandomNumberGeneratorService.generator.initialSeed = $seed"  sherpa_QCD_pt${slice}_${tag}_LO_13TeV_MASTER_cff_py_GEN.py
+#process.RandomNumberGeneratorService.generator.initialSeed = 15582
+#process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = 15582
+
+
 
 echo Starting cmsRun
 cmsRun    sherpa_QCD_pt${slice}_${tag}_LO_13TeV_MASTER_cff_py_GEN.py
